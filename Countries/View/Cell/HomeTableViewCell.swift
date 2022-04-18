08abee1df2b610f4e51122b701e21495469428f6
Ismail Palali <ismail.palali.pp@gmail.com>
2022-04-19 01:25:46 +0300

    Add Tableview - Cell  & add extension view

---
+++ Countries/View/Cell/HomeTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  HomeTableViewCell.swift
+//  Countries
+//
+//  Created by ismail palali on 19.04.2022.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var countryName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 5
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+  
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    }
+}
